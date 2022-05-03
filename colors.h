44c851cfdd9ca2dbc26769f430fdd2ae61c4baaf
leha-code <pezleha@duck.com>
2022-05-03 15:31:28 -0400

    Add the rest of the header files

---
+++ colors.h
@@ -0,0 +1,27 @@
+/*
+ * This file is part of PulpoBoy Template Game.
+ *
+ * PulpoBoy Template Game is free software: you can redistribute it and/or modify it under the terms of the 
+ * GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or 
+ * (at your option) any later version.
+ *
+ * PulpoBoy Template Game is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Foobar. If not, see 
+ * <https://www.gnu.org/licenses/>.
+ * 
+ */
+
+// Shortcuts for colors
+#define  RED     ST7735_RED
+#define  BLUE    ST7735_BLUE
+#define  YELLOW  ST7735_YELLOW
+#define  ORANGE  ST7735_ORANGE
+#define  GREEN   ST7735_GREEN
+#define  LIME    ST7735_LIME
+#define  PURPLE  ST7735_PURPLE
+#define  PINK    ST7735_PINK
+#define  MAGENTA ST7735_MAGENTA
+#define  BROWN   ST7735_BROWN
---
+++ game.h
@@ -0,0 +1,39 @@
+/*
+ * This file is part of PulpoBoy Template Game.
+ *
+ * PulpoBoy Template Game is free software: you can redistribute it and/or modify it under the terms of the 
+ * GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or 
+ * (at your option) any later version.
+ *
+ * PulpoBoy Template Game is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Foobar. If not, see 
+ * <https://www.gnu.org/licenses/>.
+ * 
+ */
+
+#include "colors.h"
+
+
+
+void setup_game(){
+  
+}
+
+void display_game_logo(){
+  // Leaving black because no game logo
+}
+
+void update_game(){
+  
+}
+
+void draw_game(){
+  
+}
+
+void delay_game(){
+  
+}
